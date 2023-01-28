418b7ada1879c5d2540a805ab340df3ce6c421d6
Dusk <65917000+duskdev17@users.noreply.github.com>
2023-01-28 15:34:17 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import {useEffect} from "react";
+
+const API_URL = 'https://www.omdbapi.com?apikey=f9b9c0bb';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch('${API_URL}&s=${title}');
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
+
+
     return(
-        <h1>Hello World!</h1>
+        <h1>My App</h1>
     );
 }
 
