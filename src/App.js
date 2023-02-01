8add2c976524221ed0768f06ac58da118febed79
Dusk <65917000+duskdev17@users.noreply.github.com>
2023-02-02 01:38:01 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=f9b9c0bb';
+//const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=f9b9c0bb';
 
 const App = () => {
 
@@ -82,10 +82,52 @@ const App = () => {
 
 
 
-    //Javascript Maps
-    
+    // //Javascript Maps
+    // const keyboards = new Map([
+    //     ['Corsair', 1000],
+    //     ['Razer', 2000],
+    //     ['Royal Kludge', 3000],
+    //     ['Keychron', 5000],
+    //     ['Dareu', 1000]
+    // ]);
+
+    // return(
+    //     <>Keyboards: {keyboards.get('Razer')}</>
+    // )
 
     
+
+    // //JavaScript Sets
+    // const myName = new Set();
+
+    // myName.add('A');
+    // myName.add('n');
+    // myName.add('i');
+    // myName.add('k');
+
+    // return(
+    //     <>My name is: {myName}</>
+    // )
+
+
+
+    //JavaScript Classes
+    class Car{
+        constructor(name, year){
+            this.name = name;
+            this.year = year;
+        }
+    }
+
+    const myCar1 = new Car('BMW', 2015);
+    const myCar2 = new Car('Audi', 2016);
+
+
+    return(
+        <>My cars are {myCar1.name} and {myCar2.name}. 
+        I bought {myCar1.name} in {myCar1.year} and {myCar2.name} in {myCar2.year}.</>
+    )
+
 
 
 }
